a74201134c438fb0c3b208ebe43be63551cb5291
Andiso67 <75083911+Andiso67@users.noreply.github.com>
2021-01-25 20:12:43 +0100

    commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,48 +4,48 @@
 
 # Connect to VMware vSphere vCenter
 provider "vsphere" {
-user = var.vsphere-user
-password = var.vsphere-password
-vsphere_server = var.vsphere-vcenter
+  user = var.vsphere-user
+  password = var.vsphere-password
+  vsphere_server = var.vsphere-vcenter
 
 # If you have a self-signed cert
-allow_unverified_ssl = var.vsphere-unverified-ssl
+  allow_unverified_ssl = var.vsphere-unverified-ssl
 }
 
 # Define VMware vSphere
 data "vsphere_datacenter" "dc" {
-name = var.vsphere-datacenter
+  name = var.vsphere-datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-name = var.vm-datastore
-datacenter_id = data.vsphere_datacenter.dc.id
+  name = var.vm-datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-name = var.vsphere-cluster
-datacenter_id = data.vsphere_datacenter.dc.id
+  name = var.vsphere-cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-name = var.vm-network
-datacenter_id = data.vsphere_datacenter.dc.id
+  name = var.vm-network
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-name = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
-datacenter_id = data.vsphere_datacenter.dc.id
+  name = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Create VMs
 resource "vsphere_virtual_machine" "vm" {
-count = var.vm-count
-name = "${var.vm-name}-${count.index + 1}"
-resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-datastore_id = data.vsphere_datastore.datastore.id
-num_cpus = var.vm-cpu
-memory = var.vm-ram
-guest_id = var.vm-guest-id
+  count = var.vm-count
+  name = "${var.vm-name}-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id = data.vsphere_datastore.datastore.id
+  num_cpus = var.vm-cpu
+  memory = var.vm-ram
+  guest_id = var.vm-guest-id
 }
 
 network_interface {
@@ -69,5 +69,4 @@ clone {
     
     network_interface {}
   }
- }
 }
