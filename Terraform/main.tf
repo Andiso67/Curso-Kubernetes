a91860485a381e38fd019e53831bb9470956d3a1
Andiso67 <75083911+Andiso67@users.noreply.github.com>
2021-01-25 20:21:47 +0100

    commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -48,27 +48,29 @@ resource "vsphere_virtual_machine" "vm" {
   num_cpus = var.vm-cpu
   memory = var.vm-ram
   guest_id = var.vm-guest-id
-}
 
-network_interface {
-  network_id = data.vsphere_network.network.id
-}
 
-disk {
-  label = "${var.vm-name}-${count.index + 1}-disk"
-  size  = 25
-}
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
 
-clone {
-  template_uuid = data.vsphere_virtual_machine.template.id
-  customize {
-    timeout = 0
+  disk {
+    label = "${var.vm-name}-${count.index + 1}-disk"
+    size  = 25
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  
+    customize {
+      timeout = 0
     
-    linux_options {
-      host_name = "node-${count.index + 1}"
-      domain = var.vm-domain
+      linux_options {
+        host_name = "node-${count.index + 1}"
+        domain = var.vm-domain
+      }
+    
+      network_interface {}
     }
-    
-    network_interface {}
   }
 }
